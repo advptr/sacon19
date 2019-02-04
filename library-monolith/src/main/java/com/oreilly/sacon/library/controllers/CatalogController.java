d768dff94f191f7da14548359a44201aae6c4e74
Peter Larsson <peter.larsson@callistaenterprise.se>
2019-02-04 15:22:24 -0500

    Add tests to CatalogController

--- library-monolith/src/main/java/com/oreilly/sacon/library/controllers/CatalogController.java
+++ library-monolith/src/main/java/com/oreilly/sacon/library/controllers/CatalogController.java
@@ -3,16 +3,15 @@ package com.oreilly.sacon.library.controllers;
 import com.oreilly.sacon.library.dao.Item;
 import com.oreilly.sacon.library.models.Book;
 import com.oreilly.sacon.library.repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CatalogController {
@@ -22,8 +21,8 @@ public class CatalogController {
 
     @RequestMapping("/catalog")
     public String catalog(ModelMap model) {
-        Iterable<Item> items = bookRepository.findAll();
-        List<Book> books = StreamSupport.stream(items.spliterator(), false)
+        List<Book> books = bookRepository.findAll()
+                .stream()
                 .map(item -> new Book(item.getId(), item.getName(), item.getAuthor(), item.getDescription(), item.getRating(),
                         item.getImagePath(), item.isAvailable()))
                 .sorted(Comparator.comparingLong(item -> item.getId()))
@@ -32,10 +31,9 @@ public class CatalogController {
         return "catalog";
     }
 
-    @RequestMapping(value = "/catalog/borrow", params={"bookId"})
-    public String borrow(final HttpServletRequest request) {
-        String parameter = request.getParameter("bookId");
-        Item book = bookRepository.findOne(Long.valueOf(parameter));
+    @PostMapping(value = "/catalog/borrow")
+    public String borrow(@Param("bookId") Long bookId) {
+        Item book = bookRepository.findOne(bookId);
         book.setAvailable(false);
         bookRepository.save(book);
         return "redirect:/catalog";
--- library-monolith/src/main/java/com/oreilly/sacon/library/repositories/BookRepository.java
+++ library-monolith/src/main/java/com/oreilly/sacon/library/repositories/BookRepository.java
@@ -1,7 +1,8 @@
 package com.oreilly.sacon.library.repositories;
 
 import com.oreilly.sacon.library.dao.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Item, Long> {
+public interface BookRepository extends JpaRepository<Item, Long> {
 }
